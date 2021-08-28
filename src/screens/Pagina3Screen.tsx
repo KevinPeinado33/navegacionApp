1634248a9ea3a4a613db6844e9dbb84edb11f957
kpeinado <kpeinado@everis.com>
2021-08-27 22:32:49 -0500

    creacion de stacks

---
+++ src/screens/Pagina3Screen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Text, View } from 'react-native';
+
+export const Pagina3Screen = () => {
+    return (
+        <View>
+            <Text> Pagina3Screen </Text>
+        </View>
+    )
+}
